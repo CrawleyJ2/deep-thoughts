ebf20b3708a81f5323dcd8007c82738389f281c1
CrawleyJ2 <79888659+CrawleyJ2@users.noreply.github.com>
2021-09-01 11:59:43 -0400

    reaction form added to singlethought page

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -4,6 +4,8 @@ import { useQuery } from '@apollo/client';
 
 import { QUERY_THOUGHT } from '../utils/queries';
 import ReactionList from '../components/ReactionList';
+import Auth from '../utils/auth';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -32,6 +34,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
